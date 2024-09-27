8e87b498ac416103d88787bb25e88eedde9f5ae6
lissetterg <alegnaguacamole@gmail.com>
2024-09-27 14:58:36 -0600

    Create nuevacal.c

---
+++ nuevacal.c
@@ -0,0 +1,60 @@
+/*Angela Lissette Reyes Gómez
+ejercicio 4 Calculadora con funciones
+Practica 7 27/09/24*/
+
+#include <stdio.h>
+#include <math.h>
+
+int resp;
+
+int suma (int a, int b)
+{
+ resp = a + b;
+  return(resp);
+  }
+int suma2 (int a, int b);
+{
+  return(a + b);
+}
+int resta(int a - int b)
+{
+resp =  a- b;
+return(resp);
+}
+int multiplicacion(int a, int b)
+{
+  resp = a * b;
+  return(resp);
+}
+int division(int a, int b)
+{
+  resp = a/b;
+  return(resp);
+}
+int exp(int a, int b)
+{
+  resp = pow(a,b);
+  return(resp);
+}
+int main ()
+{
+  int a = 0, b = 0;
+  int respuesta;
+  printf("dime que operacion deseas realizar: \n");
+
+
+
+  switch(opcion)
+    {
+      case 1:
+        respuesta = suma(a,b);
+        break;
+
+      case 2:
+      respuesta = resta (a,b);
+      break;
+      case 3:
+      respuesta = multiplicación(a
+    }
+}
+
